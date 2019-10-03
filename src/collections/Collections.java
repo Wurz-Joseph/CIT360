382b2945a272ebe33c9afbae76fb65d6a5b11bce
Joseph Wurz <wur17002@byui.edu>
2019-10-02 19:41:30 -0500

    ArrayList, HashSet, and TreeSet

--- src/collections/Collections.java
+++ src/collections/Collections.java
@@ -1,10 +1,35 @@
 package collections;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.TreeSet;
+
 public class Collections {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String[] teams = { "Michigan Wolverines", "BYU Cougars", "Texas Longhorns", "LSU Tigers"};
+		
+		//cannot add to a standard array
+		//teams.add("Georgia Bulldogs");
+		System.out.println("This is a standard array: " + teams);
+		
+		//populate list with teams in array
+		List<String> teamsList = new ArrayList<String>(Arrays.asList(teams));
+		System.out.println("This is a list: " + teamsList);
+		teamsList.add("Michigan Wolverines");
+		teamsList.add("BYU Cougars");
+		System.out.println("List after adding elements: " + teamsList);
+		
+		//unordered set
+		HashSet teamsSet = new HashSet(teamsList);
+		System.out.println("This is an unordered HashSet: " + teamsSet);
+		
+		//ordered set
+		TreeSet teamsTree = new TreeSet(teamsList);
+		System.out.println("This is an ordered TreeSet: " + teamsTree);
+		teamsList.add("BYU Cougars");
 	}
-
 }
