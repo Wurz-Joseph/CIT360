b6bb842854ebfdfa93c0dcf69f7f22c3d1478ee6
Joseph Wurz <wur17002@byui.edu>
2019-10-07 11:01:02 -0500

    fixed formatting

--- src/collections/Collections.java
+++ src/collections/Collections.java
@@ -20,62 +20,59 @@ public class Collections {
 
 		System.out.println("This is a standard array: " + teams + "\n");
 
-		//populate list with teams in array
+		// populate list with teams in array
 		List<String> teamsList = new ArrayList<String>(Arrays.asList(teams));
-		
-		//print list
+
+		// print list
 		System.out.println("This is a List: " + teamsList + "\n");
 		teamsList.add("Michigan Wolverines");
 		teamsList.add("BYU Cougars");
-		
-		//print list with added elements
+
+		// print list with added elements
 		System.out.println("List after adding elements: " + teamsList + "\n");
-		
-		//populate set with list elements. HashSet is an unordered set 
+
+		// populate set with list elements. HashSet is an unordered set
 		Set<String> teamsSet = new HashSet<String>(teamsList);
 		System.out.println("This is an unsorted Set: " + teamsSet + "\n");
-		//sorted set
+		// sorted set
 		TreeSet<String> teamsTree = new TreeSet<String>(teamsList);
 		System.out.println("This is a sorted Set: " + teamsTree + "\n");
 		teamsList.add("BYU Cougars");
-		
-		//key/value unsorted
+
+		// key/value unsorted
 		Map<String, Integer> teamHashWins = new HashMap<>();
-		teamHashWins.put( "Michigan Wolverines", 956);
-		teamHashWins.put( "BYU Cougars", 577);
-		teamHashWins.put( "Texas Longhorns", 911);
-		teamHashWins.put( "TCU Horned Frogs", 653);
-		teamHashWins.put( "Notre Dame Fighting Irish", 900);
+		teamHashWins.put("Michigan Wolverines", 956);
+		teamHashWins.put("BYU Cougars", 577);
+		teamHashWins.put("Texas Longhorns", 911);
+		teamHashWins.put("TCU Horned Frogs", 653);
+		teamHashWins.put("Notre Dame Fighting Irish", 900);
 		System.out.println("This is an unsorted Map " + teamHashWins + "\n");
-		//key/value sorted
+		// key/value sorted
 		Map<String, Integer> teamTreeWins = new TreeMap<>();
-		teamTreeWins.put( "Michigan Wolverines", 956);
-		teamTreeWins.put( "BYU Cougars", 577);
-		teamTreeWins.put( "Texas Longhorns", 911);
-		teamTreeWins.put( "TCU Horned Frogs", 653);
-		teamTreeWins.put( "Notre Dame Fighting Irish", 900);
-		//no duplicates
-		teamTreeWins.put( "Michigan Wolverines", 940);
-		
-		
+		teamTreeWins.put("Michigan Wolverines", 956);
+		teamTreeWins.put("BYU Cougars", 577);
+		teamTreeWins.put("Texas Longhorns", 911);
+		teamTreeWins.put("TCU Horned Frogs", 653);
+		teamTreeWins.put("Notre Dame Fighting Irish", 900);
+		// no duplicates
+		teamTreeWins.put("Michigan Wolverines", 940);
+
 		System.out.println("This is a sorted Map" + teamTreeWins + "\n");
-		//queue
+		// queue
 		Queue<Integer> q = new LinkedList<>();
-	    for (int i=0; i<10; i++) 
-	     q.add(i);
+		for (int i = 0; i < 10; i++)
+			q.add(i);
 
-	    // Display contents of the queue. 
-	    System.out.println("Elements of queue: "+q); 
-	  
-	    // Removes first element of queue. 
-	    int remove = q.remove(); 
-	    System.out.println("Removed element: " + remove); 
-	  
-	    System.out.println(q); 
-	
-	    int size = q.size(); 
-	    System.out.println("Size of queue: " + size); 
-		
+		// Display contents of the queue.
+		System.out.println("Elements of queue: " + q);
+
+		// Removes first element of queue.
+		int remove = q.remove();
+		System.out.println("Removed element: " + remove);
+
+		System.out.println(q);
+
+		int size = q.size();
+		System.out.println("Size of queue: " + size);
 	}
-
 }
