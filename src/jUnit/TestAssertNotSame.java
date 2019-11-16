7324343aa6ab64012fccb931c1ca1eb8c02a29c3
Joseph Wurz <wur17002@byui.edu>
2019-11-15 19:55:37 -0600

    typo fixed

--- src/jUnit/TestAssertNotSame.java
+++ src/jUnit/TestAssertNotSame.java
@@ -6,7 +6,8 @@ import org.junit.Test;
 
 public class TestAssertNotSame {
 
-	//Checks that the two objects are different. Even thought they have the exact same values these are two separate objects
+	/*Checks that the two objects are different. 
+	  Even though they have the exact same values these are two separate objects so the test will pass.*/
 	@Test
 	public void test() {
 		Ballplayer player1 = new Ballplayer("Cubs", 168, 589);
